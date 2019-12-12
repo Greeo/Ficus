b02b02a1c2fae896e5bc40ac194b9895e302d669
Omar Greene <ogreene53@gmail.com>
2019-12-12 04:48:42 -0500

    Updates

---
+++ precache-manifest.98219a3d4970ec17aa5a8f45cc74c3da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39b651cc8a42313e3d001a7df671d1d",
+    "url": "/ficus/index.html"
+  },
+  {
+    "revision": "be7789552d3be0cc82b9",
+    "url": "/ficus/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bd0f9e8473295fb49034",
+    "url": "/ficus/static/css/main.94431eb2.chunk.css"
+  },
+  {
+    "revision": "be7789552d3be0cc82b9",
+    "url": "/ficus/static/js/2.a0103554.chunk.js"
+  },
+  {
+    "revision": "2d9ed128fe13398eb7a35188b79b0cae",
+    "url": "/ficus/static/js/2.a0103554.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd0f9e8473295fb49034",
+    "url": "/ficus/static/js/main.863e987f.chunk.js"
+  },
+  {
+    "revision": "4cd5512a48039faf9fe6",
+    "url": "/ficus/static/js/runtime-main.977ecb1a.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/ficus/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/ficus/static/media/clouds.61c4e000.png"
+  }
+]);
